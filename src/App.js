76c8b58dcc25ede3515943e4c28c3b7a149a10e5
Don Chen <donchen@Dons-MacBook-Pro.local>
2020-04-09 19:54:07 -0700

    Implement basic layout of pokedex, create pokemonCard and pokemonList components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/layouts/NavBar.js';
 import './App.css';
+import Dashboard from './components/layouts/Dashboard.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <div className="container">
+        <Dashboard/>
+      </div>
     </div>
   );
 }
---
+++ src/components/pokemon/PokemonList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import PokemonCard from './PokemonCard';
+
+
+export default class PokemonList extends Component {
+    state = {
+        url: 'https://pokeapi.co/api/v2/pokemon/',
+        pokemon: null
+    }
+    async componentDidMount() {
+        const res = await axios.get(this.state.url)
+        this.setState({
+            pokemon: res.data['results']
+        })
+    }
+    render() {
+        return (
+            <div className="row">
+                {this.state.pokemon.map( pokemon => (
+                    <PokemonCard pokemon={pokemon}/>
+                ))}
+            </div>
+        )
+    }
+}
